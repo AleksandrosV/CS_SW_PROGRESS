87e65f23b155d141aabdc8649c53238adba44b33
Aleksandros Vuros <vurosa@aforbes.com>
2025-01-24 22:53:46 +0200

    Add project files.

---
+++ CS_SW_PROGRESS/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CS_SW_PROGRESS
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
